88dd11c885e73be9a69b240dd02adb2fb12b2fa3
Prabisha01 <prabishakhadka2000@gmail.com>
2024-07-11 23:02:18 +0545

    deleted file

--- frontend-AdoptionHub/src/apis/Api.js
+++ frontend-AdoptionHub/src/apis/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://localhost:3000",
   withCredentials: true,
   headers: {
     "Content-Type": "multipart/form-data",
